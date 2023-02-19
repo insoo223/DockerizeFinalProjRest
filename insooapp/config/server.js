2fe840b87c92923bd99961b46a459e36b4df6f95
Insoo Kim <insoo@hotmail.com>
2023-02-19 11:15:05 +0000

    insooapp

---
+++ insooapp/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7ae75f083c6dfb6ad54ab49079563a7'),
+    },
+  },
+});
---
+++ insooapp/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '94ef2379-40b0-4c47-b683-2917b05345cf'
+};
